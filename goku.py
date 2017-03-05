11723644241ec4321c276d7cb77df7c938a8b91d
airetupal <icamargo.ingeniero@gmail.com>
2017-03-05 12:06:01 -0500

    Cod
    Chic@s si hay mejoras haganlas :)

--- goku.py
+++ goku.py
@@ -1,2 +1,14 @@
 #prueba ivan
 #prueba alex
+___________________________
+
+>>> import math
+>>> Vel_Inicial = int(input("Ingrese la velocidad con que sale la habilidad de Goku: "))
+>>> gravedad = float(9.8)
+>>> Angulo_Salida = int(input("Ingrese el angulo con que sale la habilidad de Goku recpecto al eje Y: "))
+>>> tmax = ((2*Vel_Inicial)*(math.sin(Angulo_Salida)))/gravedad
+>>> hmax = ((Vel_Inicial**2)*((math.sin(Angulo_Salida))**2))/2*gravedad
+>>> xmax = ((2*(Vel_Inicial**2))*((math.cos(Angulo_Salida))**2))/gravedad*(math.tan(Angulo_Salida))
+>>> print (str(tmax))
+>>> print (str(hmax))
+>>> print (str(xmax))
